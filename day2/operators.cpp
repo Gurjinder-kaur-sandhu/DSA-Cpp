1446ec578ced2765edeb943f3bb4a5a8cfd9b9b8
Gurjinder Kaur Sandhu <sandhu77guri89@gmail.com>
2026-02-18 17:21:02 +0530

    Day 2 C++ programs for type casting and operators
    - Practiced implicit and explicit type casting
    - Implemented examples demonstrating data type conversion
    - Covered arithmetic, relational, logical, and assignment operators
    - Focused on understanding expression evaluation and operator behavior

---
+++ day2/operators.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+// arithmetic operator ..... 
+//1 binary(+-*/) 
+int a,b;
+cin>>a>>b;
+cout<<a+b<<endl;
+cout<<a-b<<endl;
+cout<<a*b<<endl;
+cout<<a/b<<endl;
+cout<<a%b<<endl;
+//2( unary ++,--)
+int x=7;
+cout<<x++<<endl;
+cout<<x--<<endl;
+cout<<++x<<endl;//works same
+
+int y=10,z;
+//z=y++;
+//cout<<z<<endl;
+z=++y;
+cout<<z;
+
+
+// assignment
+int i;
+i-=5;
+cout<<i;
+
+// relational(==,>=)
+
+//logical operatos(&&,||,!)
+}
---
+++ day2/typecasting.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//constants
+const int num=25;
+//int num =35;// error
+cout<<num <<endl;
+int num1=30;
+ num1=25;
+cout<< num1 <<endl;
+
+
+//typecasting
+
+//bool-char-int-float-double
+cout<<(23.5 + 2 + 'a') << endl;//implicit
+
+cout<<(bool)3+2<< endl;// explicit  ( it consider non zero numbers as 1 )
+
+cout<<(int)('A');//65 
+
+
+
+
+
+}
